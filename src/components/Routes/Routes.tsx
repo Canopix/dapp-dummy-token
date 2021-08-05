e33a7f5b4e1c38a55cce9180fa782a77a8c63acb
Emanuel Canova <canova.9400@gmail.com>
2021-08-05 18:32:38 -0300

    Update - Remove App / Add routes

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -3,13 +3,14 @@ import { createBrowserHistory } from 'history'
 import { Switch, Route, Router } from 'react-router-dom'
 import { Layout } from '../Layout'
 import { Home } from '../HomePage'
+import { routes } from '../../routes/routes'
 
 const history = createBrowserHistory()
 const Routes = () => (
   <Router history={history}>
     <Layout>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path={routes.home()} component={Home} />
       </Switch>
     </Layout>
   </Router>
---
+++ src/routes/routes.js
@@ -0,0 +1,5 @@
+export const routes = {
+    home: () => '/',
+    balance: () => '/balance',
+    transfer: () => '/transfer',
+}
