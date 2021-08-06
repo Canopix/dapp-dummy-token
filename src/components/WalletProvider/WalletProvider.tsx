f9f95b4c0d782bf7188e4937903699369d1cd7cc
Emanuel Canova <canova.9400@gmail.com>
2021-08-06 13:23:40 -0300

    Update WalletProvider - Handle border case related with ChainId & Network

--- src/components/WalletProvider/WalletProvider.tsx
+++ src/components/WalletProvider/WalletProvider.tsx
@@ -4,13 +4,17 @@ import { Props } from './WalletProvider.types'
 import './WalletProvider.css'
 
 const WalletProvider = (props: Props) => {
-  const { isInjected } = useStoreState((state) => state.wallet)
+  const { isInjected, chainId } = useStoreState((state) => state.wallet)
   const walletActions = useStoreActions((state) => state.wallet)
   const { children } = props
 
   useEffect(() => {
-    if (typeof (window as any).ethereum !== 'undefined')
+    if (typeof (window as any).ethereum !== 'undefined') {
+      (window as any).ethereum.on('chainChanged', () =>
+        walletActions.requestChainId()
+      )
       walletActions.setIsInjected(true)
+    }
   }, [walletActions])
 
   if (!isInjected) {
@@ -19,6 +23,17 @@ const WalletProvider = (props: Props) => {
         <div className="wallet-warning">
           We are not able to detect any wallet on your browser
           <br /> Please consider to install Metamask
+        </div>
+      </div>
+    )
+  }
+
+  if (isInjected && chainId !== '0x3') {
+    return (
+      <div id="wallet-container">
+        <div className="wallet-warning">
+          We detect that you are on other network
+          <br /> Please change it to Ropsten
         </div>
       </div>
     )
